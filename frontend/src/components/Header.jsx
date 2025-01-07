ff2ec243f0d436f6306bfbaccbbc3b6479128cfe
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-01-07 23:54:57 +0200

    DevTrack: Files: Header.jsx. [frontend-header]  | Changes in: Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+   <header>
+        {/*container*/}
+        <div>
+            <div>
+                {/*logo*/}
+                <Link to={"/"} >
+                      <span className='font-[900] text-[24px]'>Home<span className='font-[600] text-[24px]'>stead</span></span>
+                </Link>
+                {/*navigation bar*/}
+                <div className=''>
+                    {/*Desktop*/}
+                </div>
+            </div>
+        </div>
+   </header>
   )
 }
 
