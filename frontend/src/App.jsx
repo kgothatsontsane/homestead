0cd8e9fca51c69bb545bedaad3b88bd78c1917c3
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-01-07 17:33:54 +0200

    Ready to build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
   return (
     <h1>Hello Kgothatso</h1>
   )
 }
-
-export default App
