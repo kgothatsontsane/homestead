05685186963196b536ea6e0c248a2b27078ab035
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-01-09 01:19:21 +0200

    Update Navbar component to use NavLink for Listings and Add Property

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import { MdHomeWork } from 'react-icons/md'
+import { MdHomeWork, MdOutlineListAlt, MdAddHome } from 'react-icons/md'
+
 const Navbar = () => {
   return (
     <nav className="hidden md:flex gap-x-4">
-        <NavLink to={"/"} className={({ isActive }) => isActive ? "active-link flexCenter gap-x-1 rounded-xl px-2 py-1" : "flexCenter gap-x-1 rounded-md px-2 py-1"}>
+        <NavLink to={"/"} className={({ isActive })=> isActive ? "active-link flexCenter gap-x-1 rounded-xl px-2 py-1" : "flexCenter gap-x-1 rounded-md px-2 py-1"}>
         <MdHomeWork />
         <div>Home</div>
         </NavLink>
-        <a href="/listings" className="text-slate-900 hover:text-slate-700">Listings</a>
-        <a href="/add-property" className="text-slate-900 hover:text-slate-700">Add Property</a>
-        <a href="/bookings" className="text-slate-900 hover:text-slate-700">Bookings</a>
-        <a href="/favourites" className="text-slate-900 hover:text-slate-700">Favourites</a>
+        <NavLink to={"/listings"} className={({ isActive }) => isActive ? "active-link flexCenter gap-x-1 rounded-xl px-2 py-1" : "flexCenter gap-x-1 rounded-md px-2 py-1"}>
+        <MdOutlineListAlt />
+        <div>Listings</div>
+        </NavLink>
+        <NavLink to={"add-property"} className={({ isActive }) => isActive ? "active-link flexCenter gap-x-1 rounded-xl px-2 py-1" : "flexCenter gap-x-1 rounded-md px-2 py-1"}>
+        <MdAddHome />
+        <div>Add Property</div>
+        </NavLink>
     </nav>
   )
 }
