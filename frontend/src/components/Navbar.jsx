bf149844af2953687025132a5f1f24087e1b18fc
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-01-09 01:03:28 +0200

    DevTrack: Files: Navbar.jsx, package.json, chunk-H6BSJVLB.js, chunk-WHGUC2DB.js, react-dom_client.js, react-icons_md.js, react-router-dom.js, react_jsx-dev-runtime.js, react_jsx-runtime.js. [frontend-header]  | Changes in: Navbar.jsx; package.json (added import); chunk-H6BSJVLB.js (added import); chunk-WHGUC2DB.js (added import); react-dom_client.js (added import); react-icons_md.js (added import); react-router-dom.js (added import); react_jsx-dev-runtime.js (added import); react_jsx-runtime.js (added import)

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-
+import { MdHomeWork } from 'react-icons/md'
 const Navbar = () => {
   return (
     <nav className="hidden md:flex gap-x-4">
-          <NavLink to={"/"} className={({isActive})"active-link flexCenter gap-x-1 rounded-md" : ""}>Home
+        <NavLink to={"/"} className={({ isActive }) => isActive ? "active-link flexCenter gap-x-1 rounded-xl px-2 py-1" : "flexCenter gap-x-1 rounded-md px-2 py-1"}>
+        <MdHomeWork />
+        <div>Home</div>
         </NavLink>
         <a href="/listings" className="text-slate-900 hover:text-slate-700">Listings</a>
         <a href="/add-property" className="text-slate-900 hover:text-slate-700">Add Property</a>
