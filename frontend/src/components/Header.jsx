1a062212acb6d0d546a4fbb6b98349c9e025a5a6
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-01-08 19:21:59 +0200

    DevTrack: Files: Header.jsx. [frontend-header]  | Changes in: Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,21 +5,21 @@ import Navbar from './Navbar'
 const Header = () => {
   return (
    <header className='max-padd-container fixed top-1 w-full left-0 right-0 z-[50]'>
-        {/*container*/}
+        {/* Container */}
         <div className='max-padd-container transition-all duration-200 rounded-3xl px-5 ring-1 ring-slate-900/5 '>
             <div className='flexBetween py-4 '>
-                {/*logo*/}
+                {/* Logo */}
                 <Link to={"/"} >
                       <span className='font-[900] text-[24px]'>Home<span className='font-[600] text-[24px]'>stead</span></span>
                 </Link>
-                {/*navigation bar*/}
-                <div className=''>
-                    {/*Desktop*/}
+                {/* Navigation bar */}
+                <div className="">
+                    {/* Desktop */}
                     <Navbar />
-                    {/*Mobile*/}
-                    <Navbar />
+                    {/* Mobile */}
+                    {/* <Navbar /> */}
                 </div>
-                {/*buttons*/}
+                {/* Buttons */}
                 <div>
                     buttons and icons
                 </div>
