97fff3043914fbfeb8bc459cd4d8ef4ea32572d2
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-01-10 01:44:53 +0200

    DevTrack: Files: Navbar.jsx. [frontend-header]  | Changes in: Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,31 +1,31 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import { MdHomeWork, MdOutlineListAlt, MdAddHome, MdOutlineAddHome, MdOutlineHomeWork, MdListAlt } from 'react-icons/md'
+import { MdHomeWork, MdViewQuilt, MdAddHome, MdContactSupport } from 'react-icons/md'
 
 const Navbar = ({ containerStyles }) => {
-  return (
-    <nav className={`${containerStyles}`}>
-        <NavLink to={"/"} className={({ isActive })=> isActive ? "active-link flexCenter gap-x-1 rounded-xl px-2 py-1" : "flexCenter gap-x-1 rounded-md px-2 py-1"}>
-        <MdHomeWork />
-        <div>Welcome</div>
-        </NavLink>
-        <NavLink to={"/listings"} className={({ isActive }) => isActive ? "active-link flexCenter gap-x-1 rounded-xl px-2 py-1" : "flexCenter gap-x-1 rounded-md px-2 py-1"}>
-        <MdListAlt />
-        <div>Listings</div>
-        </NavLink>
-        
-        <NavLink to={"/listings"} className={({ isActive }) => isActive ? "active-link flexCenter gap-x-1 rounded-xl px-2 py-1" : "flexCenter gap-x-1 rounded-md px-2 py-1"}>
-        <MdAddHome />
-        <div>Add Home</div>
-        </NavLink>
-        
-        
-        <NavLink to={"add-property"} className={({ isActive }) => isActive ? "active-link flexCenter gap-x-1 rounded-xl px-2 py-1" : "flexCenter gap-x-1 rounded-md px-2 py-1"}>
-        <MdContactSupport />
-        <div>Contact</div>
-        </NavLink>
-    </nav>
-  )
+    return (
+        <nav className={`${containerStyles}`}>
+            <NavLink to={"/"} className={({ isActive }) => isActive ? "active-link flexCenter gap-x-1 rounded px-2 py-1.5" : "flexCenter gap-x-1 rounded px-2 py-1.5"}>
+                <MdHomeWork />
+                <div>Welcome</div>
+            </NavLink>
+            <NavLink to={"/listings"} className={({ isActive }) => isActive ? "active-link flexCenter gap-x-1 rounded px-2 py-1.5" : "flexCenter gap-x-1 rounded px-2 py-1.5"}>
+                <MdViewQuilt />
+                <div>Listings</div>
+            </NavLink>
+
+            <NavLink to={"/add-property"} className={({ isActive }) => isActive ? "active-link flexCenter gap-x-1 rounded px-2 py-1.5" : "flexCenter gap-x-1 rounded px-2 py-1.5"}>
+                <MdAddHome />
+                <div>Add Home</div>
+            </NavLink>
+
+
+            <NavLink to={"/contact"} className={({ isActive }) => isActive ? "active-link flexCenter gap-x-1 rounded px-2 py-1.5" : "flexCenter gap-x-1 rounded px-2 py-1.5"}>
+                <MdContactSupport />
+                <div>Contact</div>
+            </NavLink>
+        </nav>
+    )
 }
 
 export default Navbar
