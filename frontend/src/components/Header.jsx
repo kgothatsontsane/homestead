3364d6e391745c0a4ad7a895fcea4954fc81a356
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-01-13 21:31:36 +0200

    DevTrack: Files: Header.jsx. [frontend-header]  | Changes in: Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
    <header className='max-padd-container fixed top-1 w-full left-0 right-0 z-[50]'>
         {/* Container */}
-        <div className='max-padd-container transition-all duration-200 rounded-3xl px-5 ring-1 ring-slate-900/5 '>
+        <div className='max-padd-container transition-all duration-200 rounded px-5 ring-1 ring-slate-900/5 '>
             <div className='flexBetween py-4 '>
                 {/* Logo */}
                 <Link to={"/"} >
