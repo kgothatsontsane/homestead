1029ad649c3031a7273eaaad4e4007e7ca629628
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-01-20 01:08:11 +0200

    DevTrack: Files: About.jsx, Home.jsx. [home-about-section]  | Changes in: About.jsx (added import); Home.jsx

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import aboutImg from "../assets/about.jpg"
+
+const About = () => {
+  return (
+    <section>
+        <div>
+            <div>
+                <img src={aboutImg} alt="About Us" />
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default About
