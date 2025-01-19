4597e7c83998b4fea4f35abad7778f4a95749cee
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-01-19 15:32:01 +0200

    DevTrack: Initial commit with existing project files

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Hero = () => {
   return (
     <section className='max-padd-container pt-[99px]'>
-        <div className='max-padd-container bg-hero bg-center ng-cover bg-no-repeat h-[655px] w-full rounded-xl items-center'>
+        <div className='max-padd-container bg-hero bg-center bg-cover bg-no-repeat h-[655px] w-full rounded-xl items-center'>
             <div className='relative top-32 xs:top-52'>
                 <span className='medium-18'>Welcome to Homestead</span>
                 <h1 className='h1 capitalize max-w-[40rem]'>Discover Your Dream Home With Homestead</h1>
@@ -13,10 +13,10 @@ const Hero = () => {
                 {/* View Listings Button */}
                 <div className='inline-flex items-center justify-center gap-4 p-2 bg-white rounded-xl'>
                     <div className='text-center regular-14 leading-tight pl-5'>
-                        <h5>Valuation Report</h5>
-                        <p>On All Properties</p>
+                        <h5 className='uppercase font-bold'>Valuation Report</h5>
+                        <p className='regular-14'>On All Properties</p>
                     </div>
-                    <Link to="/listings">View Homes</Link>
+                    <Link to="/listings" className='btn-secondary rounded-xl'>View Homes</Link>
                 </div>
             </div>
         </div>
