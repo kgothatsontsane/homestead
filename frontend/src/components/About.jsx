485a4e3a0fc3b61e7f1b7da6575690981d0b7b52
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-01-21 18:48:27 +0200

    DevTrack: Files: About.jsx. [home-about-section]  | Changes in: About.jsx

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -4,21 +4,19 @@ import { MdFormatQuote } from 'react-icons/md'
 
 const About = () => {
   return (
-    <section>
-        <div>
-            <div>
-                <img src={aboutImg} alt="About Us" className='rounded-xl rounded-t-[155px] w-[488px]'/>
-                <div>
-                    <span>
+    <section className='max-padd-container py-16 xl:py-28'>
+        <div className='flex flex-col xl:flex-row items-center justify-between gap-10'>
+            <div className='flex-1 relative'>
+                <img src={aboutImg} alt="About Us" className='rounded-full w-[488px]'/>
+                  <div className='bg-white absolute bottom-16 left-16 shadow-md max-w-xs max-h-xs p-4 rounded-full flexCenter flex-col'>
+                    <span className='relative bottom-8 p-3 shadow-md bg-white '>
                         <MdFormatQuote className='text-2xl'/>
-                        <span>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                            </p>
-                                <h3>John Doe</h3>
-                                <p>CEO</p>
-                        </span>
                     </span>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.strud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                    <h3>John Doe</h3>
+                    <p>CEO</p>       
                 </div>
             </div>
         </div>
