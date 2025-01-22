4aa21af586ea0d663d059d01e26279689646ea32
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-01-22 13:53:06 +0200

    DevTrack: Files: About.jsx. [home-about-section]  | Changes in: About.jsx

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -7,18 +7,17 @@ const About = () => {
     <section className='max-padd-container py-16 xl:py-28'>
         <div className='flex flex-col xl:flex-row items-center justify-between gap-10'>
             <div className='flex-1 relative'>
-                <img src={aboutImg} alt="About Us" className='rounded-full w-[488px]'/>
+                <img src={aboutImg} alt="About Us" className='rounded-3xl w-[488px]'/>
                   <div className='bg-white absolute bottom-16 left-16 shadow-md max-w-xs max-h-xs p-4 rounded-xl flexCenter flex-col'>
-                    <span className='relative bottom-8 p-3 shadow-md bg-white '>
+                    <span className='relative bottom-8 p-3 shadow-md bg-white h-12 w-12 rounded-full flex items-center'>
                         <MdFormatQuote className='text-2xl'/>
                     </span>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.strud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
-                    <h3>John Doe</h3>
-                    <p>CEO</p>       
+                    <p className='text-center relative bottom-3'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.strud exercitation ullamco laboris nisi ut aliquip ex ea commodo.
+                    </p>     
                 </div>
             </div>
+            {/}
         </div>
     </section>
   )
