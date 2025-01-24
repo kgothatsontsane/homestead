2e7fe4f94d41d9868554db41a3870db164986527
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-01-24 11:32:30 +0200

    DevTrack: Files: Item.jsx, FavButton.jsx. [home-properties-section]  | Changes in: Item.jsx; FavButton.jsx

--- frontend/src/components/FavButton.jsx
+++ frontend/src/components/FavButton.jsx
@@ -4,7 +4,7 @@ import { FaHeart } from 'react-icons/fa6'
 
 const FavButton = () => {
   return (
-    <FaHeart color='white' size={22} className='cursor-pointer drop-shadow-sm'/>
+    <FaHeart color='white' size={22} className='cursor-pointer drop-shadow-md'/>
     
   )
 }
--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -1,22 +1,34 @@
 import React from 'react'
 import FavButton from './FavButton'
-import { MdOutlineBed } from 'react-icons/md'
+import { MdOutlineBathtub, MdOutlineBed, MdOutlineGarage } from 'react-icons/md'
+import { Link } from 'react-router-dom'
 
 const Item = ({ property }) => {
+  const formatPrice = (price) => {
+    return `R ${price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}`;
+  };
+
   return (
-    <div>
-        <div>
-            <img src={property.image} alt={property.title} className='rounded-xl w-full'/>
+    <div className='rounded-xl p-5 bg-white'>
+        <div className='pb-2 relative'>
+            <img src={property.image} alt={property.title} className='rounded-lg w-full'/>
               {/* Like Button */}
             <div className='absolute top-4 right-4'>
                 <FavButton />
             </div>
         </div>
-        <h5>{property.city}</h5>
+        <h5 className='bold-16 my-1 '>{property.city}</h5>
         <h4>{property.title}</h4>
         {/* Property Info */}
-        <div className='flex gap-2 py-2'>
-            <div className='flexCenters gap-x-2 border-r border-slate-900/50 pr-4 font-[500]'><MdOutlineBed /> {property.facilities.bedrooms}</div>
+        <div className='flex gap-x-2 py-2'>
+            <div className='flexCenter gap-x-2 border-r border-slate-900/50 pr-4 font-[500]'><MdOutlineBed /> {property.facilities.bedTubMdOutlineBathtubs}</div>
+            <div className='flexCenter gap-x-2 border-r border-slate-900/50 pr-4 font-[500]'><MdOutlineBathtub /> {property.facilities.bathTubMdOutlineBathtubs}</div>
+            <div className='flexCenter gap-x-2 border-r border-slate-900/50 pr-4 font-[500]'><MdOutlineGarage /> {property.facilities.parkkings}</div>
+        </div>
+        <p>{property.description}</p>
+        <div className='flexBetween'>
+            <div>{formatPrice(property.price)}</div>
+            <Link to={`/property/${property.title}`} className='btn-secondary !py-2 !px-4 rounded-xl shadow-ms'>View Property</Link>
         </div>
     </div>
   )
