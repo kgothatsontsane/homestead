b191e344a92c7a4bea9737664b287c246b95e53b
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-01-24 11:02:29 +0200

    DevTrack: Files: Item.jsx, FavButton.jsx. [home-properties-section]  | Changes in: Item.jsx; FavButton.jsx (added import)

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import FavButton from './FavButton'
+import { MdOutlineBed } from 'react-icons/md'
 
 const Item = ({ property }) => {
   return (
@@ -6,9 +8,15 @@ const Item = ({ property }) => {
         <div>
             <img src={property.image} alt={property.title} className='rounded-xl w-full'/>
               {/* Like Button */}
-            <div>
-                <Fvo 
+            <div className='absolute top-4 right-4'>
+                <FavButton />
             </div>
+        </div>
+        <h5>{property.city}</h5>
+        <h4>{property.title}</h4>
+        {/* Property Info */}
+        <div className='flex gap-2 py-2'>
+            <div className='flexCenters gap-x-2 border-r border-slate-900/50 pr-4 font-[500]'><MdOutlineBed /> {property.facilities.bedrooms}</div>
         </div>
     </div>
   )
