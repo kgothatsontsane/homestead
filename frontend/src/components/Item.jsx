5554bcb3064beb29d6906aba76f42ec3a5993934
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-01-24 12:16:29 +0200

    Enhance Item component layout and styling for improved readability and consistency

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -17,18 +17,22 @@ const Item = ({ property }) => {
                 <FavButton />
             </div>
         </div>
-        <h5 className='bold-16 my-1 '>{property.city}</h5>
-        <h4>{property.title}</h4>
+        <h5 className='bold-16 my-1 text-secondary'>{property.city}</h5>
+        <h4 className='medium-18 line-clamp-1 '>{property.title}</h4>
         {/* Property Info */}
         <div className='flex gap-x-2 py-2'>
-            <div className='flexCenter gap-x-2 border-r border-slate-900/50 pr-4 font-[500]'><MdOutlineBed /> {property.facilities.bedTubMdOutlineBathtubs}</div>
-            <div className='flexCenter gap-x-2 border-r border-slate-900/50 pr-4 font-[500]'><MdOutlineBathtub /> {property.facilities.bathTubMdOutlineBathtubs}</div>
-            <div className='flexCenter gap-x-2 border-r border-slate-900/50 pr-4 font-[500]'><MdOutlineGarage /> {property.facilities.parkkings}</div>
+            <div className='flexCenter gap-x-2 border-r border-slate-900/50 pr-4 font-[500]'><MdOutlineBed /> {property.facilities.bedrooms}</div>
+            <div className='flexCenter gap-x-2 border-r border-slate-900/50 pr-4 font-[500]'><MdOutlineBathtub /> {property.facilities.bathrooms}</div>
+            <div className='flexCenter gap-x-2 border-r border-slate-900/50 pr-4 font-[500]'><MdOutlineGarage /> {property.facilities.parkings}</div>
         </div>
-        <p>{property.description}</p>
-        <div className='flexBetween'>
-            <div>{formatPrice(property.price)}</div>
-            <Link to={`/property/${property.title}`} className='btn-secondary !py-2 !px-4 rounded-xl shadow-ms'>View Property</Link>
+        <p className='pt-2 mb-4 line-clamp-2'>{property.description}</p>
+        <div className='block'>
+            <div className='bold-20 xl:line-clamp-none'>{formatPrice(property.price)}</div>
+            <Link to={`/`}>
+                  <button className='btn-secondary rounded-lg shadow-ms w-full mt-2'>
+                    View Property
+                </button>
+            </Link>
         </div>
     </div>
   )
