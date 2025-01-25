1ea0f28da685fbbf458b962bffe836b80db7f0d3
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-01-25 20:01:44 +0200

    Initialize Express server with dotenv, cookie-parser, and CORS middleware

--- backend/index.js
+++ backend/index.js
@@ -3,3 +3,16 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
