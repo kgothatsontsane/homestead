1fe1eee85b0dd892c4cff1408f906bad5b983e44
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-01-25 15:30:09 +0200

    Add Listings page: implement property listing with Searchbar and Item components

--- frontend/src/pages/Listings.jsx
+++ frontend/src/pages/Listings.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Searchbar from '../components/Searchbar'
+import { PROPERTIES } from '../constant/data'
+import Item from '../components/Item'
 
 const Listings = () => {
   return (
-    <div className='flexCenter h-[999px]'>Listings</div>
-  )
+    <main className='max-padd-container my-[99px]'>
+      <div className='max-padd-container py-10 xl:py-22 bg-primary rounded-xl'>
+        <div>
+          <Searchbar />
+          {/* container */}
+          <div className='grid gap-6 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mt-12'>
+            {PROPERTIES.map((property) => (
+                <Item key={property.title} property={property} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Listings
