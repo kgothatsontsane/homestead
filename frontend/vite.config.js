b70e005f9ea9e0ddafa3e6d3da105a2e7a8127e7
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-01-27 17:14:54 +0200

    Add Babel plugin configuration for React in Vite setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const ReactCompilerConfig = { /* ... */ };
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: [
+        ["babel-plugin-react-compiler", ReactCompilerConfig]
+      ]
+    }
+  })],
 })
