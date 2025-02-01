63b183b6d9ee315b69d9fd7aa1463d7039512508
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-02-01 15:47:40 +0200

    Enhance Layout component by adding Header and Footer for improved structure

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <>
+      <div>
+        <Header/>
+        <Outlet/>
+      </div>
+      <Footer/>
+    </>
   )
 }
 
