c4f1e716ae5e802ff4969129bb4c32b7f4801e20
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-02-02 22:05:28 +0200

    Refactor useProperties hook: update useQuery syntax to use queryKey and queryFn options

--- frontend/src/hooks/useProperties.jsx
+++ frontend/src/hooks/useProperties.jsx
@@ -2,14 +2,11 @@ import React from 'react'
 import { useQuery } from '@tanstack/react-query'
 
 const useProperties = () => {
-
-  const { data, isError, isLoading, refetch } = useQuery(
-    'allProperties',
-    getAllProperties,
-    {
-      refetchOnWindowFocus: false
-    }
-  )
+  const { data, isError, isLoading, refetch } = useQuery({
+    queryKey: ['allProperties'],
+    queryFn: getAllProperties,
+    refetchOnWindowFocus: false
+  })
   
   return (
     data, isError, isLoading, refetch
