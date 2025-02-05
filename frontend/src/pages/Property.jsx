c43b5ae0bbe1f9173f8ba25064ae813c505b188b
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-02-05 14:25:49 +0200

    Fetch property data using React Query in Property component

--- frontend/src/pages/Property.jsx
+++ frontend/src/pages/Property.jsx
@@ -1,6 +1,20 @@
+import { useLocation } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react'
+import { getProperty } from '../utils/api';
 
 const Property = () => {
+
+  const { pathname } = useLocation();
+  const id = pathname.split('/').slice(-1)[0]
+  console.log(id)
+  
+  
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["property", id],
+    queryFn: () => getProperty(id),
+  });
+  console.log(data)
   return (
     <div>Property</div>
   )
