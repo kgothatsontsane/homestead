40a5a63ad25bb9cd186618533a3e2cfeb122568e
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-02-08 20:26:32 +0200

    Add theme hook for managing color variables

---
+++ frontend/src/hooks/useTheme.js
@@ -0,0 +1,9 @@
+export const useTheme = () => {
+  return {
+    primary: "rgb(var(--color-primary) / <alpha-value>)",
+    secondary: "rgb(var(--color-secondary) / <alpha-value>)",
+    tertiary: "rgb(var(--color-tertiary) / <alpha-value>)",
+    white: "#FFFFFF",
+    black: "#000000",
+  };
+};
