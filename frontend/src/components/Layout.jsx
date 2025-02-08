4f9744e30d82d9cba3358644609d1884bbe2f5d8
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-02-08 20:28:41 +0200

    Refactor Layout component to improve structure with a flexbox layout and ensure proper footer positioning

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -6,12 +6,14 @@ import ScrollUpButton from './ScrollUpButton'
 
 const Layout = () => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <Outlet />
-      <Footer />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer className="z-10 relative bg-white" />
       <ScrollUpButton />
-    </>
+    </div>
   )
 }
 
