048cbe9eb5da6696e726beb2b3c54cb85ab9a512
Kgothatso Ntsane <nvisionfactory@gmail.com>
2025-02-08 20:26:42 +0200

    Update Tailwind CSS configuration with new primary and tertiary colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,37 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {
-            colors: {
-                primary: "#f5f6f2",
-                secondary: "#1454f1",
-                tertiary: "#222222",
-                secondaryRed: "#f42c37",
-                secondaryYellow: "#fdc62e",
-                secondaryGreen: "#2dcc6f",
-                secondaryBlue: "#1376f4",
-                secondaryWhite: "#eeeeee",
-                gray: {
-                    10: "#EEEEEE",
-                    20: "#A2A2A2",
-                    30: "#7B7B7B",
-                    50: "#585858",
-                    90: "#141414",
-                },
-            },
-            screens: {
-                xs: "400px",
-                "3xl": "1680px",
-                "4xl": "2200px",
-            },
-            backgroundImage: {
-                hero: "url(/src/assets/bg.jpg)",
-            },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3B82F6",
+        secondary: "#1454f1",
+        tertiary: "#1E3A8A",
+        secondaryRed: "#f42c37",
+        secondaryYellow: "#fdc62e",
+        secondaryGreen: "#2dcc6f",
+        secondaryBlue: "#1376f4",
+        secondaryWhite: "#eeeeee",
+        gray: {
+          10: "#EEEEEE",
+          20: "#A2A2A2",
+          30: "#7B7B7B",
+          50: "#585858",
+          90: "#141414",
         },
+      },
+      screens: {
+        xs: "400px",
+        "3xl": "1680px",
+        "4xl": "2200px",
+      },
+      backgroundImage: {
+        hero: "url(/src/assets/bg.jpg)",
+      },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
